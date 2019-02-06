c2410754fb8fab4abb8a218a04f716ad449f48c1
Blueoctopusinc <blueoctopusink@gmail.com>
2019-02-06 15:05:32 +0000

    Imports
    Imported and included interfaces for the angular router and nativestorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { NativeStorageOriginal } from '@ionic-native/native-storage';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +13,22 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private nativeStorage: NativeStorageOriginal,
+    private router: Router
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      this.nativeStorage.getItem('facebook_user')
+      .then( data => {
+        this.router.navigate(['/home']);
+      }, err => {
+        this.router.navigate(['/login']);
+
+      });
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
